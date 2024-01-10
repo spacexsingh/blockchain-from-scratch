d263607e2235df8b96be3b7a23ce1ac77eabe1b1
triumph-isingh <120404365+triumph-isingh@users.noreply.github.com>
2024-01-10 14:25:30 +0800

    Added functionality to Toggle the input

--- src/c1_state_machine/p1_switches.rs
+++ src/c1_state_machine/p1_switches.rs
@@ -15,7 +15,8 @@ impl StateMachine for LightSwitch {
 	type Transition = ();
 
 	fn next_state(starting_state: &bool, t: &()) -> bool {
-		todo!("Exercise 1")
+		let x = !*starting_state;
+		x
 	}
 }
 
